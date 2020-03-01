3a4876c3535bf13849e9e0e60fd4c6315ae026f9
= <=>
2020-02-29 21:46:51 -0300

    Foi feito as lestras da música Stressed Out e foi alterado o Readme.md

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Musica_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**stressed out**");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wish we could turn back time, to the good old days");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("When our momma sang us to sleep but now we're stressed out");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Wish we could turn back time, to the good old days");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("When our momma sang us to sleep but now we're stressed out");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+        }
+    }
+}
